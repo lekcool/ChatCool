1e39709be962b42ecec65abe52d890b00d34387a
Cool <lek_0335@hotmail.com>
2017-04-01 19:09:38 +0700

    update

--- app/src/main/java/com/pun/cool/chatcool/config/Config.java
+++ app/src/main/java/com/pun/cool/chatcool/config/Config.java
@@ -22,6 +22,7 @@ public class Config {
     public static final int PUSH_TYPE_CHAT_ROOM = 1;
     public static final int PUSH_TYPE_USER = 2;
 
+    // test comment
     // id to handle the notification in the notification try
     public static final int NOTIFICATION_ID = 100;
     public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
